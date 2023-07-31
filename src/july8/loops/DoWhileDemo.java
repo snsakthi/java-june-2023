4dd08f5c2c48b3da1c1ad79904fa67385b1c7b4e
snsakthi <snithyasakthi@gmail.com>
2023-07-31 17:38:22 -0400

    added all new files

---
+++ src/july8/loops/DoWhileDemo.java
@@ -0,0 +1,16 @@
+package july8.loops;
+
+public class DoWhileDemo {
+    public static void main(String[] args) {
+
+        int i =0, n=10;
+
+
+        do {
+            System.out.println(i);
+            i++;
+
+
+        } while (i <= n) ;
+    }
+}
---
+++ src/july8/loops/LoopDemo2.java
@@ -0,0 +1,10 @@
+package july8.loops;
+
+public class LoopDemo2 {
+    public static void main(String[] args) {
+        int counter = 0;
+        while(counter>=10){ //false statement ..infinite loop
+            System.out.println("pragra");
+        }
+    }
+}
